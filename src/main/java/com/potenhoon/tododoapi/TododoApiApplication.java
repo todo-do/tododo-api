76aeaab5c03e482bbd84f912d2a56e816c314650
JSeHoone <beebea545@gmail.com>
2025-11-25 15:57:30 +0900

    first commit

---
+++ src/main/java/com/potenhoon/tododoapi/TododoApiApplication.java
@@ -0,0 +1,13 @@
+package com.potenhoon.tododoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TododoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TododoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/potenhoon/tododoapi/TododoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potenhoon.tododoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TododoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
