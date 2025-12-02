126b5c7961aae1b13daa39d06d2a5d48981a378e
minji0828 <entersuk1998@naver.com>
2025-12-02 11:11:46 +0900

    fix[Character] stats dto 수정
    a, b, c -> stat로 수정

--- src/main/java/com/potenhoon/tododoapi/character/presentation/dto/StatsRequest.java
+++ src/main/java/com/potenhoon/tododoapi/character/presentation/dto/StatsRequest.java
@@ -2,8 +2,8 @@ package com.potenhoon.tododoapi.character.presentation.dto;
 
 import com.potenhoon.tododoapi.character.domain.model.Stats;
 
-public record StatsRequest(int a, int b, int c, int d, int e, int f) {
+public record StatsRequest(int stat) {
     public Stats toStats() {
-        return Stats.createStat(a, b, c, d, e, f);
+        return Stats.createStat(0);
     }
 }
--- src/main/java/com/potenhoon/tododoapi/character/presentation/dto/StatsResponse.java
+++ src/main/java/com/potenhoon/tododoapi/character/presentation/dto/StatsResponse.java
@@ -2,15 +2,10 @@ package com.potenhoon.tododoapi.character.presentation.dto;
 
 import com.potenhoon.tododoapi.character.domain.model.Stats;
 
-public record StatsResponse(int a, int b, int c, int d, int e, int f) {
+public record StatsResponse(int stat) {
     public static StatsResponse from(Stats stats) {
         return new StatsResponse(
-                stats.getA(),
-                stats.getB(),
-                stats.getC(),
-                stats.getD(),
-                stats.getE(),
-                stats.getF()
-        );
+                stats.getStat());
+
     }
 }
